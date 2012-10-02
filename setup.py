439185fc386c7508a74010a1bc3d761400cb5557
Sean Davis <seandavi@gmail.com>
2012-10-02 18:15:20 -0400

    Added varscanFix.py to scripts and added setuptools build script

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os, glob
+
+version = '0.1.0'
+
+setup(name='seqtools',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      scripts=glob.glob('scripts/*'),
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
