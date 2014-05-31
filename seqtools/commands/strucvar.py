2eecdf1c26f5e17f57aa2acd04b80b60eb461595
Sean Davis <seandavi@gmail.com>
2014-05-31 09:23:50 -0400

    Added strucvar and crestToBed subcommands and removed old single script

---
+++ seqtools/commands/strucvar.py
@@ -0,0 +1,24 @@
+from seqtools.main import subparsers
+import seqtools.strucvar
+import seqtools.strucvar.crest
+
+def crestToBed(opts):
+    with open(opts.predSVfile,'r') as f:
+        for line in f:
+            if(opts.samplename is not None):
+                print(seqtools.strucvar.crest.crestLineToBedLines(line,opts.samplename))
+            else:
+                print(seqtools.strucvar.crest.crestLineToBedLines(line))
+                
+
+
+strucvar_parser = subparsers.add_parser('strucvar')
+strucvar_subparsers = strucvar_parser.add_subparsers(help="Structural variation subcommands")
+crestToBed_parser = strucvar_subparsers.add_parser('crestToBed',
+                                                   help="convert CREST output to two BED records per line")
+crestToBed_parser.add_argument('-s','--samplename',default=None,type=str,
+                               help="sample information to add to the BED 'name' column")
+crestToBed_parser.add_argument('predSVfile',help="The name of the CREST predSV.txt file")
+
+crestToBed_parser.set_defaults(func=crestToBed)
+
---
+++ seqtools/strucvar/crest.py
@@ -0,0 +1,15 @@
+def crestLineToBedLines(crestline,extrastring=None):
+    """Takes a line from a CREST file and turns it into two BED lines
+
+    :param crestline: a single string representing the CREST output
+    :param extrastring: a single string to concatenate to the bed output, useful for including sample information, etc.
+    
+    Returns a string containing the two bed lines"""
+    sline = crestline.strip().split("\t")
+    outstringparts=[sline[8],sline[0],sline[1],sline[4],sline[5]]
+    if(extrastring is not None):
+        outstringparts+=[str(extrastring)]
+    namefield = "_".join(outstringparts)
+    leftbreak = "\t".join([sline[0],sline[1],sline[1],namefield])
+    rightbreak = "\t".join([sline[4],sline[5],sline[5],namefield])
+    return(leftbreak+"\n"+rightbreak)
