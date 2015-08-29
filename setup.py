f2fbd18adafd14ab2ed47438f80feac4284d670f
Sean Davis <seandavi@gmail.com>
2015-08-29 12:59:36 -0400

    Added readme and updated travis to push to pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 import sys, os, glob
 
-version = '0.9.1'
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = '0.9.3'
 
 setup(name='SDST',
       version=version,
       description="Useful routines to perform NGS data analysis tasks",
-      long_description="""\
-""",
+      long_description=read('README.md'),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Sean Davis',
       author_email='seandavi@gmail.com',
-      url='https://github.com/seandavi/seqtools',
+      url='https://github.com/seandavi/SDST',
       license='MIT',
       scripts=glob.glob('scripts/*'),
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
